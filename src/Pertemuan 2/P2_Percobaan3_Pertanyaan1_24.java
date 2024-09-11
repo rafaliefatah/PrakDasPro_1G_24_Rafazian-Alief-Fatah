fcad1af3784a4f12913a4b537b6c57c6ee284abb
rafaliefatah <rafazian336@gmail.com>
2024-09-11 21:37:37 +0700

    identitas diri

---
+++ src/Pertemuan 2/P2_Percobaan3_Pertanyaan1_24.java
@@ -0,0 +1,16 @@
+public class P2_Percobaan3_Pertanyaan1_24 {
+    
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi-POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Rafazian Alief Fatah");
+        System.out.println("NIM     : 244107020100");
+        System.out.println("Kelas   : 1G");
+        System.out.println("No Absen: 24");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Sendang Biru RT10 RW03 Tambakrejo, Sumbermanjing Wetan, Kab. Malang");
+        System.out.println("No HP   : 083833843906");
+        System.out.println("Hobi    : Bermain game dan mendengarkan musik");
+    }
+}
