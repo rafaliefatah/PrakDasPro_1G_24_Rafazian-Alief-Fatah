2d70c85a68fb0c2e8d5c39c3e29260218fb18e8c
Rafazian Alief Fatah <rafazian336@gmail.com>
2024-09-11 21:27:22 +0700

    Add files via upload

---
+++ src/Pertemuan 2/P2_Percobaan3_Pertanyaan1_24.java
@@ -0,0 +1,24 @@
+public class P2_Percobaan3_Pertanyaan1_24 {
+    public static void main(String[] args) {
+        System.out.println("                                                                           KARTU RENCANA STUDI (KRS)\n");
+        System.out.println("NIM                     : 244107020100");
+        System.out.println("NAMA                    : RAFAZIAN ALIEF FATAH");
+        System.out.println("PROGRAM STUDI           : D-IV  TEKNIK INFORMATIKA");
+        System.out.println("KELAS                   : 1G\n");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("|        KODE MK        |                                                        MATA KULIAH                                                   |    SKS    |   JAM   |");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("|       RTI2410001      |Pancasila                                                                                                             |     2     |    2    |");
+        System.out.println("|       RTI2410002      |Konsep Teknologi Informasi                                                                                            |     2     |    4    |");
+        System.out.println("|       RTI2410003      |Critical Thinking dan Problem Solving                                                                                 |     2     |    4    |");
+        System.out.println("|       RTI2410004      |Matematika Dasar                                                                                                      |     3     |    6    |");
+        System.out.println("|       RTI2410005      |Rekayasa Perangkat Lunak                                                                                              |     2     |    4    |");
+        System.out.println("|       RTI2410006      |Dasar Pemrograman                                                                                                     |     2     |    4    |");
+        System.out.println("|       RTI2410007      |Praktikum Dasar Pemrograman                                                                                           |     3     |    6    |");
+        System.out.println("|       RTI2410008      |Keselamatan dan Kesehatan Kerja                                                                                       |     2     |    4    |");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("|                       |                                                           JUMLAH                                                     |     18    |    34   |");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+    }
+}
