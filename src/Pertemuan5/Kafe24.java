285d2d13bcc278331644e65f9b907faf69b4c219
rafaliefatah <rafazian336@gmail.com>
2024-10-02 23:05:56 +0700

    P5_Tugas2

---
+++ src/Pertemuan5/Kafe24.java
@@ -0,0 +1,36 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class Kafe24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean keanggotaan;
+        int jmlKopi,jmlTeh,jmlRoti;
+        float diskon = 10/100f;
+        double hrgKopi = 12000.0,hrgTeh = 7000.0,hrgRoti = 20000.0,nominalByr,totalHrg;
+
+        System.out.print("Masukkan keanggotaan (true/false): ");
+        keanggotaan = input.nextBoolean();
+        System.out.print("Masukkan jumlah pembelian kopi: ");
+        jmlKopi = input.nextInt();
+        System.out.print("Masukkan jumlah pembelian teh: ");
+        jmlTeh = input.nextInt();
+        System.out.print("Masukkan jumlah pembelian roti: ");
+        jmlRoti = input.nextInt();
+
+        totalHrg = (hrgKopi*jmlKopi)+(hrgTeh*jmlTeh)+(hrgRoti*jmlRoti);
+
+        if (keanggotaan) {
+            nominalByr = totalHrg-(diskon*totalHrg);
+        } else {
+            nominalByr = totalHrg;
+        }
+
+        int nominalInt = (int) nominalByr;
+
+        System.out.println("Keanggotaan pelanggan: "+keanggotaan);
+        System.out.println("Item pembelian: "+jmlKopi+" kopi, "+jmlTeh+" teh, "+jmlRoti+" roti");
+        System.out.println("Nominal bayar: Rp."+nominalInt);
+    }
+}
