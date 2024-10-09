70ea617b7620c9377ac9cad4bd3039c5e2f7a7c3
rafaliefatah <rafazian336@gmail.com>
2024-10-09 21:14:42 +0700

    P6_Percobaan1

---
+++ src/Pertemuan6/Pemilihan2Percobaan124.java
@@ -0,0 +1,20 @@
+package Pertemuan6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan124 {
+    public static void main(String[] args) {
+        Scanner input24 = new Scanner (System.in);
+        int tahun;
+        
+        System.out.print("Masukkan tahun: ");
+        tahun = input24.nextInt();
+
+        if ((tahun%4)==0) {
+            if ((tahun%100)!=0)
+            System.out.println("Tahun kabisat");
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
