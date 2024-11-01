a68f489444ceb788f236bf1bb99f9d1a3b59be73
rafaliefatah <rafazian336@gmail.com>
2024-11-01 15:38:19 +0700

    P10_Percobaan2_PertanyaanNo5

--- src/Pertemuan10/ArrayNilai24.java
+++ src/Pertemuan10/ArrayNilai24.java
@@ -7,12 +7,16 @@ public class ArrayNilai24 {
         Scanner sc24 = new Scanner (System.in);
         int nilaiAkhir[] = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+": ");
             nilaiAkhir[i] = sc24.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i]>70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
     }
 }
