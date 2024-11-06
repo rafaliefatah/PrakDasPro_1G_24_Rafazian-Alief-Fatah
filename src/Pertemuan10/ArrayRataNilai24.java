b72b6774bb6921361d2f952eda6263eef0b065e2
rafaliefatah <rafazian336@gmail.com>
2024-11-06 10:09:02 +0700

    P10_Percobaan3_PertanyaanNo3

--- src/Pertemuan10/ArrayRataNilai24.java
+++ src/Pertemuan10/ArrayRataNilai24.java
@@ -5,17 +5,26 @@ import java.util.Scanner;
 public class ArrayRataNilai24 {
     public static void main(String[] args) {
         Scanner sc24 = new Scanner (System.in);
-        int nilaiMhs[] = new int [10];
-        double total=0,rata2;
+        int nilaiMhs[],lulus=0,tidakLulus=0, jumlahMhs;
+        double totalLulus=0,totalTidakLulus=0,rataLulus,rataTidakLulus;
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jumlahMhs = sc24.nextInt();
+        nilaiMhs = new int [jumlahMhs];
+        for (int i = 0; i < jumlahMhs; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
             nilaiMhs[i] = sc24.nextInt();
+            if (nilaiMhs[i]>70) {
+                totalLulus+=nilaiMhs[i];
+                lulus++;
+            } else {
+                totalTidakLulus+=nilaiMhs[i];
+                tidakLulus++;
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        rataLulus = totalLulus/lulus;
+        System.out.println("Rata-rata nilai lulus = "+rataLulus);
+        rataTidakLulus = totalTidakLulus/tidakLulus;
+        System.out.println("Rata-rata nilai tidak lulus = "+rataTidakLulus);
     }
 }
