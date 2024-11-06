46ad855d2f6a255f4ae2af11f099a14d4aa96e75
rafaliefatah <rafazian336@gmail.com>
2024-11-06 09:16:07 +0700

    P10_Percobaan3

---
+++ src/Pertemuan10/ArrayRataNilai24.java
@@ -0,0 +1,21 @@
+package Pertemuan10;
+
+import java.util.Scanner;
+
+public class ArrayRataNilai24 {
+    public static void main(String[] args) {
+        Scanner sc24 = new Scanner (System.in);
+        int nilaiMhs[] = new int [10];
+        double total=0,rata2;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
+            nilaiMhs[i] = sc24.nextInt();
+        }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
+        rata2 = total/nilaiMhs.length;
+        System.out.println("Rata-rata nilai = "+rata2);
+    }
+}
