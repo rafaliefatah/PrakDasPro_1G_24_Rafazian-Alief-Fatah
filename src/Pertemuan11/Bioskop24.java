5e9e18a45de2a1f47d44618eea584801b4cfd5b7
rafaliefatah <rafazian336@gmail.com>
2024-11-14 08:26:13 +0700

    P11_Percobaan1

--- src/Pertemuan11/Bioskop24.java
+++ src/Pertemuan11/Bioskop24.java
@@ -10,7 +10,6 @@ public class Bioskop24 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
-        
         System.out.printf("%s\t%s\n", penonton[0][0],penonton[0][1]);
         System.out.printf("%s\t%s\n", penonton[1][0],penonton[1][1]);
         System.out.printf("%s\t%s\n", penonton[2][0],penonton[2][1]);
