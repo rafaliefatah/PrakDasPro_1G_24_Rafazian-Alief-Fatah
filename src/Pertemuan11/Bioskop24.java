2ff2ff88e8516439f8950fe008a5db028d5cd131
rafaliefatah <rafazian336@gmail.com>
2024-11-14 09:03:21 +0700

    P11_Percobaan1_PertanyaanNo14

--- src/Pertemuan11/Bioskop24.java
+++ src/Pertemuan11/Bioskop24.java
@@ -10,9 +10,20 @@ public class Bioskop24 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
-        System.out.printf("%s\t%s\n", penonton[0][0],penonton[0][1]);
-        System.out.printf("%s\t%s\n", penonton[1][0],penonton[1][1]);
-        System.out.printf("%s\t%s\n", penonton[2][0],penonton[2][1]);
-        System.out.printf("%s\t%s\n", penonton[3][0],penonton[3][1]);
+        penonton[3][1] = "Hana";
+
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: "+barisPenonton.length);
+        }
+
+        System.out.println("Penonton pada baris ke-3: ");
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+        
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-"+(i+1)+": "+String.join(", ",penonton[i]));
+        }
     }
 }
