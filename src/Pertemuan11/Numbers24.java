635b8318bba787ba5abe4b1f7283f940d3b6f87d
rafaliefatah <rafazian336@gmail.com>
2024-11-14 11:08:39 +0700

    P11_Percobaan3

---
+++ src/Pertemuan11/Numbers24.java
@@ -0,0 +1,10 @@
+package Pertemuan11;
+
+public class Numbers24 {
+    public static void main(String[] args) {
+        int myNumbers[][] = new int [3][];
+        myNumbers[0] = new int [5];
+        myNumbers[1] = new int [3];
+        myNumbers[2] = new int [1];
+    }
+}
