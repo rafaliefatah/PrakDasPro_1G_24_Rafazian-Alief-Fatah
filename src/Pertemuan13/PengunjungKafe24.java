fab67bdf495e54683bb8583fc440f360ee13e553
rafaliefatah <rafazian336@gmail.com>
2024-12-05 08:18:36 +0700

    P13_Percobaan4_PertanyaanNo2

--- src/Pertemuan13/PengunjungKafe24.java
+++ src/Pertemuan13/PengunjungKafe24.java
@@ -3,8 +3,8 @@ package Pertemuan13;
 public class PengunjungKafe24 {
     public static void daftarPengunjung(String... namaPengunjung){
         System.out.println("Daftar Nama Pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- "+namaPengunjung[i]);
+        for (String daftarNama : namaPengunjung) {
+            System.out.println("- "+daftarNama);
         }
     }
     public static void main(String[] args) {
