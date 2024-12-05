491464d6353ffa0e75fc44fc9e105f9610a65fd9
rafaliefatah <rafazian336@gmail.com>
2024-12-05 13:47:34 +0700

    P13_Percobaan5_PertanyaanNo2

--- src/Pertemuan13/Percobaan524.java
+++ src/Pertemuan13/Percobaan524.java
@@ -18,6 +18,9 @@ public class Percobaan524 {
         System.out.println("Luas persegi panjang adalah: "+L);
         vol = hitungVolume(t, p, l);
         System.out.println("Volume balok adalah: "+vol);
+
+        int temp = Jumlah(1, 1);
+        TampilJumlah(temp, 5);
     }
     public static int hitungLuas (int pjg, int lb) {
         int Luas = pjg*lb;
@@ -26,5 +29,16 @@ public class Percobaan524 {
     public static int hitungVolume (int tinggi, int a, int b) {
         int volume = hitungLuas(a, b)*tinggi;
         return volume;
+    }
+    public static void TampilHinggaKei (int i) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print(j);
+        }
+    }
+    public static int Jumlah (int bil1, int bil2) {
+        return (bil1+bil2);
+    }
+    public static void TampilJumlah (int bil1, int bil2) {
+        TampilHinggaKei(Jumlah(bil1, bil2));
     }
 }
