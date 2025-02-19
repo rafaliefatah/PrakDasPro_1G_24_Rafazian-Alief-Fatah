97bae03e9608cccf5dbffdd85bc358c8bb825a46
rafaliefatah <rafazian336@gmail.com>
2025-02-19 22:30:17 +0700

    P2_Percobaan2

--- src/Semester 2/Pertemuan2/MahasiswaMain23.java
+++ src/Semester 2/Pertemuan2/MahasiswaMain23.java
@@ -1,40 +1,6 @@
 public class MahasiswaMain23 {
-    String nim,nama,kelas;
-    double ipk;
-
-    void tampilkanInformasi() {
-        System.out.println("Nama: "+nama);
-        System.out.println("NIM: "+nim);
-        System.out.println("IPK: "+ipk);
-        System.out.println("Kelas: "+kelas);
-    }
-
-    void ubahKelas(String kelasBaru) {
-        kelas=kelasBaru;
-    }
-
-    void updateIpk(double ipkBaru) {
-        if (ipk>=0.0 && ipk<=4.0) {
-            ipk=ipkBaru;
-        } else {
-            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
-        }
-    }
-
-    String nilaiKinerja(double ipk) {
-        if (ipk>=3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk>=3.0) {
-            return "Kinerja baik";
-        } else if (ipk>=2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
-        }
-    }
-
     public static void main(String[] args) {
-        MahasiswaMain23 mhs1 = new MahasiswaMain23();
+        Mahasiswa23 mhs1 = new Mahasiswa23();
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
