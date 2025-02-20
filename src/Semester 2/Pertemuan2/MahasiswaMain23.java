2643a9569e201381c5ad1e92dde4f5baf1e4af92
rafaliefatah <rafazian336@gmail.com>
2025-02-20 08:01:28 +0700

    P2_Percobaan3

--- src/Semester 2/Pertemuan2/MahasiswaMain23.java
+++ src/Semester 2/Pertemuan2/MahasiswaMain23.java
@@ -10,5 +10,9 @@ public class MahasiswaMain23 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa23 mhs2 = new Mahasiswa23("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
