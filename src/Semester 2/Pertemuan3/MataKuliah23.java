7c16c4d5e3979ce1a54590d3005c605d99a742cd
rafaliefatah <rafazian336@gmail.com>
2025-02-27 10:37:17 +0700

    P3_Percobaan3_Pertanyaan

--- src/Semester 2/Pertemuan3/MataKuliah23.java
+++ src/Semester 2/Pertemuan3/MataKuliah23.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class MataKuliah23 {
-    public String kode,nama;
+    Scanner sc23 = new Scanner(System.in);
+    public String kode,nama,dummy;
     public int sks,jumlahJam;
 
     public MataKuliah23 (String kode, String nama, int sks, int jumlahJam) {
@@ -7,5 +10,30 @@ public class MataKuliah23 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public MataKuliah23() {
+    }
+
+    void tambahData() {
+        System.out.print("Kode       : ");
+        kode = sc23.nextLine();
+        System.out.print("Nama       : ");
+        nama = sc23.nextLine();
+        System.out.print("SKS        : ");
+        dummy = sc23.nextLine();
+        sks = Integer.parseInt(dummy);
+        System.out.print("Jumlah Jam : ");
+        dummy = sc23.nextLine();
+        jumlahJam = Integer.parseInt(dummy);
+        System.out.println("------------------------------------");
+    }
+
+    void cetakInfo() {
+            System.out.println("Kode        : "+kode);
+            System.out.println("Nama        : "+nama);
+            System.out.println("SKS         : "+sks);
+            System.out.println("Jumlah Jam  : "+jumlahJam);
+            System.out.println("------------------------------------");    
     }
 }
--- src/Semester 2/Pertemuan3/MataKuliahDemo23.java
+++ src/Semester 2/Pertemuan3/MataKuliahDemo23.java
@@ -3,34 +3,19 @@ import java.util.Scanner;
 public class MataKuliahDemo23 {
     public static void main(String[] args) {
         Scanner sc23 = new Scanner(System.in);
-        MataKuliah23[] arrayOfMatakuliah23 = new MataKuliah23[3];
-        String kode,nama,dummy;
-        int sks,jumlahJam;
+        System.out.print("Masukkan panjang (jumlah elemen) : ");
+        int panjang = sc23.nextInt();
+        MataKuliah23[] arrayOfMatakuliah23 = new MataKuliah23[panjang];
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < panjang; i++) {
+            arrayOfMatakuliah23[i] = new MataKuliah23();
             System.out.println("Masukkan Data Mata Kuliah ke-"+(i+1));
-            System.out.print("Kode       : ");
-            kode = sc23.nextLine();
-            System.out.print("Nama       : ");
-            nama = sc23.nextLine();
-            System.out.print("SKS        : ");
-            dummy = sc23.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam : ");
-            dummy = sc23.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("------------------------------------");
-
-            arrayOfMatakuliah23[i] = new MataKuliah23(kode, nama, sks, jumlahJam);
+            arrayOfMatakuliah23[i].tambahData();
         }
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Data Mahasiswa ke-"+(i+1));
-            System.out.println("Kode        : "+arrayOfMatakuliah23[i].kode);
-            System.out.println("Nama        : "+arrayOfMatakuliah23[i].nama);
-            System.out.println("SKS         : "+arrayOfMatakuliah23[i].sks);
-            System.out.println("Jumlah Jam  : "+arrayOfMatakuliah23[i].jumlahJam);
-            System.out.println("------------------------------------");    
+        for (int i = 0; i < panjang; i++) {
+            System.out.println("Data Mata Kuliah ke-"+(i+1));
+            arrayOfMatakuliah23[i].cetakInfo();
         }
     }
 }
