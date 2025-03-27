1530659e19ebd2a9ea773d17c540e84d5e816d12
rafaliefatah <rafazian336@gmail.com>
2025-03-27 07:03:26 +0700

    P7_Percobaan1

---
+++ src/Semester 2/Pertemuan7/MahasiswaBerprestasi23.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class MahasiswaBerprestasi23 {
+    Scanner sc23 = new Scanner(System.in);
+    Mahasiswa23 listMhs[] = new Mahasiswa23[5];
+    int idx;
+
+    void tambah(Mahasiswa23 m) {
+        if (idx<listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh");
+        }
+    }
+    
+    void tampil() {
+        for (Mahasiswa23 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("-------------------------");
+        }
+    }
+
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int i = 0; i < listMhs.length; i++) {
+            if (listMhs[i].ipk == cari) {
+                posisi=i;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data mahasiswa dengan IPK "+x+" ditemukan pada indeks "+pos);
+        } else {
+            System.out.println("Data "+x+" tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("NIM\t : "+listMhs[pos].nim);
+            System.out.println("Nama\t : "+listMhs[pos].nama);
+            System.out.println("Kelas\t : "+listMhs[pos].kelas);
+            System.out.println("IPK\t : "+x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK "+x+" tidak ditemukan");
+        }
+    }
+}
---
+++ src/Semester 2/Pertemuan7/MahasiswaDemo23.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MahasiswaDemo23 {
+    public static void main(String[] args) {
+        MahasiswaBerprestasi23 list = new MahasiswaBerprestasi23();
+        Scanner sc23 = new Scanner(System.in);
+        int jumMhs=5;
+
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan data mahasiswa ke-"+(i+1));
+            System.out.print("NIM\t : ");
+            String nim = sc23.nextLine();
+            System.out.print("Nama\t : ");
+            String nama = sc23.nextLine();
+            System.out.print("Kelas\t : ");
+            String kelas = sc23.nextLine();
+            System.out.print("IPK\t : ");
+            String ip = sc23.nextLine();
+            double ipk = Double.parseDouble(ip);
+            System.out.println("-------------------------");
+            list.tambah(new Mahasiswa23(nim, nama, kelas, ipk));
+        }
+
+        list.tampil();
+        System.out.println("-------------------------");
+        System.out.println("Pencarian Data");
+        System.out.println("-------------------------");
+        System.out.println("Masukkan IPK mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = sc23.nextDouble();
+
+        System.out.println("Menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss = (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+    }
+}
