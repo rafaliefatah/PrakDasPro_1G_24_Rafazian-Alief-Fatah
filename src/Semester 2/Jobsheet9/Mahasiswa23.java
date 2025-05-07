b29480c4f887400c8e3c41fd4a03f28bdbb9557c
rafaliefatah <rafazian336@gmail.com>
2025-05-08 04:10:12 +0700

    P10_Percobaan1

---
+++ src/Semester 2/Jobsheet9/Mahasiswa23.java
@@ -0,0 +1,16 @@
+package Jobsheet9;
+public class Mahasiswa23 {
+    String nama,nim,kelas;
+    int nilai;
+
+    Mahasiswa23(String nama, String nim, String kelas) {
+        this.nama=nama;
+        this.nim=nim;
+        this.kelas=kelas;
+        nilai=-1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai=nilai;
+    }
+}
