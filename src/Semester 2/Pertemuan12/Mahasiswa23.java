10f2d31544a7120699bbd6afda76bbd009e10215
rafaliefatah <rafazian336@gmail.com>
2025-05-15 09:24:47 +0700

    P12_Percobaan1

---
+++ src/Semester 2/Pertemuan12/Mahasiswa23.java
@@ -0,0 +1,18 @@
+public class Mahasiswa23 {
+    String nim,nama,kelas;
+    double ipk;
+
+    Mahasiswa23() {
+    }
+
+    Mahasiswa23(String nm, String name, String kls, double ip) {
+        nim=nm;
+        nama=name;
+        kelas=kls;
+        ipk=ip;
+    }
+
+    public void tampilInformasi() {
+        System.out.println(nama+"\t\t"+nim+"\t\t"+kelas+"\t\t"+ipk);
+    }
+}
