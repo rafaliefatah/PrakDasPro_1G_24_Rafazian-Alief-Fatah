77b7a694279e3e9e096289b3c7a8b8c49bc836f5
rafaliefatah <rafazian336@gmail.com>
2025-05-22 09:32:58 +0700

    P13_Percobaan3

--- src/Semester 2/doublelinkedlists23/DoubleLinkedListsMain23.java
+++ src/Semester 2/doublelinkedlists23/DoubleLinkedListsMain23.java
@@ -1,23 +1,21 @@
 public class DoubleLinkedListsMain23 {
     public static void main(String[] args) throws Exception {
         DoubleLinkedLists23 dll = new DoubleLinkedLists23();
-        dll.addLast(50);
-        dll.addLast(40);
-        dll.addLast(10);
-        dll.addLast(20);
         dll.print();
         System.out.println("Size: "+dll.size());
         System.out.println("========================");
-        dll.removeFirst();
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
         dll.print();
         System.out.println("Size: "+dll.size());
         System.out.println("========================");
-        dll.removeLast();
+        dll.add(40, 1);
         dll.print();
         System.out.println("Size: "+dll.size());
         System.out.println("========================");
-        dll.remove(1);
-        dll.print();
-        System.out.println("Size: "+dll.size());
+        System.out.println("Data awal pada linked lists adalah: "+dll.getFirst());
+        System.out.println("Data terakhir pada linked lists adalah: "+dll.getLast());
+        System.out.println("Data index ke-1 pada linked lists adalah: "+dll.get(1));
     }
 }
